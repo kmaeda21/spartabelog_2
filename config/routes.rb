ed97bc472c4bcc8f99937a2fb4efd68bfebe5924
Kay Maeda <k.maeda.21@gmail.com>
2015-06-27 17:50:20 +0900

    Routing done

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
 
   root 'restaurants#index'
 
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
